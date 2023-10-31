3ae7b4cfd1361f4cf27e7e856512ac0743e6313f
HAZEMADEL2030 <87597338+HAZEMADEL2030@users.noreply.github.com>
2023-10-31 23:46:48 +0200

    Add files via upload

---
+++ 33-Multicasting/33-Multicasting/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _33_Multicasting
+{
+    class Multicast
+    {
+        public static void Sinus(int number)
+        {
+            Console.WriteLine("Sinis is:" + Math.Sin(number));
+        }
+
+        public static void Cosinus(int number)
+        {
+            Console.WriteLine("Cosinus is:" + Math.Cos(number));
+        }
+    }
+
+    public delegate void MyDelegate(int number);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate D1 = Multicast.Sinus;
+            MyDelegate D2 = Multicast.Cosinus;
+
+            Console.WriteLine("<----Simple Delegate---->");
+            D1.Invoke(60);
+            D2(180);
+            Console.WriteLine();
+            Console.WriteLine("<---Multicast-->");
+            MyDelegate MulticastDelegate = D1 + D2;
+            MulticastDelegate(90);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 34-Eventum/34-Eventum/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _34_Eventum
+{
+    public delegate void orderDelegate();
+    class Product
+    {
+        private string Label;
+        private int Quantity;
+
+        //Event Definition
+        public event orderDelegate orderEvent;
+
+        //Constructor
+        public Product(string label, int quantity)
+        {
+            this.Label = label;
+            this.Quantity = quantity;
+        }
+
+        //Properties
+        public string Label_Property
+        {
+            get { return Label; }
+            set { Label = value; }
+        }
+
+        public int Quantity_Property
+        {
+            get { return Quantity; }
+            set { Quantity = value; }
+        }
+
+        public void order(int Nbr_Products)
+        {
+            if (Nbr_Products > this.Quantity) orderEvent();
+            this.Quantity = this.Quantity - Nbr_Products;
+        }
+
+        public void messageEvent()
+        {
+            Console.WriteLine("This Quantity is not available !");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product prod = new Product("Keyboard", 20);
+            prod.orderEvent += new orderDelegate(prod.messageEvent);
+            prod.order(60);
+            Console.WriteLine(prod.Quantity_Property);
+            Console.Read();
+        }
+    }
+}
