76dd50454c4e4e21e96cbe3e6d8f6f7f85c93da6
HAZEMADEL2030 <87597338+HAZEMADEL2030@users.noreply.github.com>
2023-10-31 23:14:50 +0200

    Add files via upload

---
+++ 13-Listing/13-Listing/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _13_Listing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare a List
+            List<int> myList = new List<int>();
+
+            //Populate a List
+            myList.Add(6);
+            myList.Add(9);
+            myList.Add(7);
+            myList.Add(2);
+
+
+            //Remove an item From a List by value:
+            myList.Remove(6);
+
+            //Remove an item From a List by index:
+            myList.RemoveAt(1);
+
+
+            //Display the items of List
+            foreach (int number in myList)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 14-Listing Example/14-Listing Example/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _14_Listing_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare a List
+            List<string> myList = new List<string>();
+
+            //Populate a List
+            myList.Add("Hazem");
+            myList.Add("Adel");
+            myList.Add("Mohamed");
+            myList.Add("Khalil");
+
+
+            //Display the items of List
+            foreach (string name in myList)
+            {
+                if (name.Length <= 6)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
