06c3a9fe0f358cffedb8e7009bd1883907e2968e
HAZEMADEL2030 <87597338+HAZEMADEL2030@users.noreply.github.com>
2023-10-31 23:11:35 +0200

    Add files via upload

---
+++ 11-Structures/11-Structures/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _11_Structures
+{
+    internal class Program
+    {
+
+        struct Person
+        {
+            public string FullName;
+            public int Age;
+
+        }
+        static void Main(string[] args)
+        {
+            Person P = new Person();
+            Console.WriteLine("Enter your Name:");
+            P.FullName = Console.ReadLine();
+
+            Console.WriteLine("Enter your Age:");
+            P.Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your Full Name is: " + P.FullName + ", and your Age is: " + P.Age);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 12-Enumerators/12-Enumerators/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _12_Enumerators
+{
+    internal class Program
+    {
+
+        enum Marital_Status
+        {
+            married,
+            single
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Are you married ?");
+            string answer = Console.ReadLine();
+
+            switch (answer)
+            {
+                case "yes":
+                    Console.WriteLine("You are: " + Marital_Status.married);
+                    break;
+                case "no":
+                    Console.WriteLine("You are: " + Marital_Status.single);
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
