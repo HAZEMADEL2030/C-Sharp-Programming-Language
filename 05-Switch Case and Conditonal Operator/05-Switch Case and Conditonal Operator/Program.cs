88541070417a5ac2cc4d7d1081050ef7bfda0aad
HAZEMADEL2030 <87597338+HAZEMADEL2030@users.noreply.github.com>
2023-10-31 23:03:39 +0200

    Add files via upload

---
+++ 05-Switch Case and Conditonal Operator/05-Switch Case and Conditonal Operator/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _05_Switch_Case_and_Conditonal_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //switch case statement
+            int number1, number2;
+            double div;
+
+            Console.WriteLine("Enter the first number:");
+            number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            number2 = int.Parse(Console.ReadLine());
+
+            switch (number2)
+            {
+                case 0:
+                    Console.WriteLine("Cannot Divide By Zero !");
+                    break;
+                default:
+                    div = number1 + number2;
+                    Console.WriteLine("The quotient is: " + div);
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 06-Loops the For Loop/06-Loops the For Loop/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _06_Loops_the_For_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //First Example:
+            for (int i = 0; i <= 9; i++)
+            {
+                Console.WriteLine("Hello !");
+            }
+
+            Console.WriteLine();
+
+            //Second Example (Multiplication Table):
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    Console.WriteLine(i + " * " + j + " = " + (i * j));
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
