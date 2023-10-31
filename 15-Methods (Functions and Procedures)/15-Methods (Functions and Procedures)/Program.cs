f4d6c7c080536dec45d6829220ee827fc12abbb3
HAZEMADEL2030 <87597338+HAZEMADEL2030@users.noreply.github.com>
2023-10-31 23:16:15 +0200

    Add files via upload

---
+++ 15-Methods (Functions and Procedures)/15-Methods (Functions and Procedures)/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _15_Methods__Functions_and_Procedures_
+{
+    internal class Program
+    {
+
+        //Create a Procedure
+        static void showMessage(string param)
+        {
+            Console.WriteLine("Welcome: " + param);
+        }
+
+        //Create a function
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            //Call a Procedure
+            showMessage("Hazem");
+
+
+            //Call a function
+            int result = Sum(20, 40);
+            Console.WriteLine("The sum is: " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 16-Mathematical Functions/16-Mathematical Functions/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _16_Mathematical_Functions
+{
+    internal class Program
+    {
+
+        //Create a function
+        static double Calcul(int a, int b, char op)
+        {
+            double result;
+            switch (op)
+            {
+
+                case '+':
+                    result = a + b;
+                    break;
+                case '-':
+                    result = a - b;
+                    break;
+                case '*':
+                    result = a * b;
+                    break;
+                case '/':
+                    result = a / b;
+                    break;
+                default:
+                    return 0;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            //Addition
+            double sum = Calcul(8, 2, '+');
+
+            //soustraction
+            double difference = Calcul(8, 2, '-');
+
+            //Multiplication
+            double Product = Calcul(8, 2, '*');
+
+            //Disvision
+            double Quotient = Calcul(8, 2, '/');
+
+            Console.WriteLine(sum + "  " + difference + "  " + Product + "  " + Quotient);
+            Console.ReadKey();
+        }
+    }
+}
