8a5b0b0424ba20fdda69d712463fbde1de7ee208
HAZEMADEL2030 <87597338+HAZEMADEL2030@users.noreply.github.com>
2023-10-31 23:08:35 +0200

    Add files via upload

---
+++ 09-Arrays - Part 2/09-Arrays - Part 2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _09_Arrays___Part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] names = new string[2, 3];
+            names[0, 0] = "Hazem";
+            names[0, 1] = "Adel";
+            names[0, 2] = "Mohamed";
+
+            names[1, 0] = "Mazen";
+            names[1, 1] = "Asem";
+            names[1, 2] = "Mostafa";
+
+            foreach (string n in names)
+            {
+                //Display all items in Upper format
+                Console.WriteLine("Welome: " + n.ToUpper());
+            }
+
+            Console.WriteLine("--------");
+
+            foreach (string n in names)
+            {
+                //Display all items in Lower format
+                Console.WriteLine("Welcome: " + n.ToLower());
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 10-Sort Array/10-Sort Array/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _10_Sort_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //One Dimensional Array
+            //First way by using the Sort Method
+
+            /*int[] numbers = { 1, 8, 6, 4, 9, 0, 12, 5 };
+            Array.Sort(numbers);
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }*/
+
+            //Second Way by an algorithm
+            int[] Array = new int[5];
+            int i, j, Temp;
+            for (i = 0; i < Array.Length; i++)
+            {
+                Console.WriteLine("Enter The Number: " + i);
+                Array[i] = int.Parse(Console.ReadLine());
+            }
+            for (i = 0; i < Array.Length; i++)
+            {
+                for (j = i + 1; j < Array.Length; j++)
+                {
+                    if (Array[i] > Array[j])
+                    {
+                        Temp = Array[i];
+                        Array[i] = Array[j];
+                        Array[j] = Temp;
+                    }
+                }
+            }
+            for (i = 0; i < Array.Length; i++)
+            {
+                Console.WriteLine("The sorted array is : " + Array[i]);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
