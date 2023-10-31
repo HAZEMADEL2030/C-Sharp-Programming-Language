c4548278737c6255fffd1406179df1e3b5c38242
HAZEMADEL2030 <87597338+HAZEMADEL2030@users.noreply.github.com>
2023-10-31 23:32:15 +0200

    Add files via upload

---
+++ 25-Properties (Getters and Setters)/25-Properties (Getters and Setters)/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace _25_Properties__Getters_and_Setters_
+{
+    class Car
+    {
+        //Fields
+        private int ID;
+        private string Color;
+        private string Model;
+
+        //Properties
+        public int ID_Property
+        {
+            get { return ID; }
+            set { this.ID = value; }
+        }
+
+        public string Color_Property
+        {
+            get { return Color; }
+            set { this.Color = value; }
+        }
+
+        public string Model_Property
+        {
+            get
+            {
+                return this.Model;
+            }
+            set
+            {
+                this.Model = value;
+            }
+        }
+        //Constructors
+        public Car(int id, string color, string model)
+        {
+            this.ID = id;
+            this.Color = color;
+            this.Model = model;
+        }
+
+        public Car()
+        {
+        }
+
+        //Methods
+        public void initialize(int id, string color, string model)
+        {
+            ID = id;
+            Color = color;
+            Model = model;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine("ID is:{0}\nColor is:{1}\nModel is:{2}", ID, Color, Model);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car(123, "Violet", "Mercedes");
+            Console.WriteLine(myCar.Model_Property);
+            myCar.Model_Property = "Peugeot";
+            Console.WriteLine(myCar.Model_Property);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 26-Inheritance/26-Inheritance/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _26_Inheritance
+{
+    //Main Class
+    class Person
+    {
+        //Fields
+        private string Name;
+        private int Age;
+        private string Address;
+
+        //Method
+        public void PrintMessage()
+        {
+            Console.WriteLine("I am a person");
+        }
+    }
+
+    class Doctor : Person
+    {
+        private string Speciality;
+    }
+
+    class Programmer : Person
+    {
+        private string Programming_Language;
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Programmer prg = new Programmer();
+            prg.PrintMessage();
+            Console.ReadKey();
+        }
+    }
+}
