0ba0ab8ede16b7082169fd91030df10f07d16132
HAZEMADEL2030 <87597338+HAZEMADEL2030@users.noreply.github.com>
2023-10-31 23:06:16 +0200

    Add files via upload

---
+++ 07-Loops Do, While, Goto/07-Loops Do, While, Goto/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _07_Loops_Do__While__Goto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Do...While
+            int number = 0;
+            do
+            {
+                Console.WriteLine("Welcome");
+                number++;
+
+            } while (number > 0 && number < 5);
+
+        //While
+        Label: Console.WriteLine("Who is the first man who landed on the Moon ?");
+            string khalifa = Console.ReadLine();
+            while (khalifa != "Armstrong")
+            {
+                Console.WriteLine("False");
+                goto Label;
+            }
+            Console.WriteLine("True");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 08-Arrays - Part 1/08-Arrays - Part 1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _08_Arrays___Part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare an array of strings
+            string[] name = new string[4];
+
+            //Populate the array
+            name[0] = "Hazem";
+            name[1] = "Adel";
+            name[2] = "Mohamed";
+            name[3] = "Khalil";
+
+            //display the items of array with for loop
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine("Welcome : " + name[i]);
+            }
+
+            Console.WriteLine();
+
+            //display the items of array with foreach loop
+            foreach (string n in name)
+            {
+                Console.WriteLine("Welcome: " + n);
+            }
+            Console.ReadKey();
+        }
+    }
+}
