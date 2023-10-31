09e793bde3a51557661c8eb23f73f1312c30465d
HAZEMADEL2030 <87597338+HAZEMADEL2030@users.noreply.github.com>
2023-10-31 23:24:36 +0200

    Add files via upload

---
+++ 21-Classes and Objects/21-Classes and Objects/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _21_Classes_and_Objects
+{
+    class Car
+    {
+        //Fields
+        int ID;
+        string Color;
+        string Model;
+
+
+        //Methods
+        public void initialize(int id, string color, string model)
+        {
+            ID = id;
+            Color = color;
+            Model = model;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine("ID is:{0}\nColor is:{1}\nModel is:{2}", ID, Color, Model);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.initialize(1, "Black", "Mercedes");
+            myCar.ShowDetails();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 22-Namespaces and Assemblies/22-Namespaces and Assemblies/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _22_Namespaces_and_Assemblies
+{
+    internal class Program
+    {
+
+        class Book
+        {
+            //Fields
+            string Title;
+            string Author;
+            string Publisher;
+
+
+            public void initilaize(string title, string author, string publisher)
+            {
+                this.Title = title;
+                this.Author = author;
+                this.Publisher = publisher;
+            }
+
+
+            public void ShowDetails()
+            {
+                Console.WriteLine("Title is:{0}\nAuthor is:{1}\nPublisher is:{2}", Title, Author, Publisher);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.initilaize("C# Programming Language", "Hazem Adel Mohamed", "International Programming");
+            book.ShowDetails();
+            Console.Read();
+        }
+    }
+}
