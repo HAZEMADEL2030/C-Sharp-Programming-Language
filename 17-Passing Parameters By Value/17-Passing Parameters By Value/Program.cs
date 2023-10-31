270b8f1a827ef22c950dd4ab754cadc2e2824e49
HAZEMADEL2030 <87597338+HAZEMADEL2030@users.noreply.github.com>
2023-10-31 23:18:23 +0200

    Add files via upload

---
+++ 17-Passing Parameters By Value/17-Passing Parameters By Value/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _17_Passing_Parameters_By_Value
+{
+    internal class Program
+    {
+
+        static void myProc(int Number)
+        {
+            Number = 6;
+            Console.WriteLine("Value during passing is :" +
+            Number);
+        }
+        static void Main(string[] args)
+        {
+            int Num = 10;
+            Console.WriteLine("Value Before passing is :" + Num);
+            myProc(Num);
+            Console.WriteLine("Value After passing is :" + Num);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 18-Passing Parameters By Reference and Output/18-Passing Parameters By Reference and Output/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _18_Passing_Parameters_By_Reference_and_Output
+{
+    internal class Program
+    {
+
+        static void myProc(ref int Number)
+        {
+            Number = 6;
+            Console.WriteLine("Value during passing is :" +
+            Number);
+        }
+
+        static void myProc2(out int Number)
+        {
+            Number = 8;
+            Console.WriteLine("Value during passing is :" +
+            Number);
+        }
+        static void Main(string[] args)
+        {
+            int Num = 10;
+            int Num2;
+            Console.WriteLine("Value Before passing is :" + Num);
+            //ref needs an initial value of parameter
+            myProc(ref Num);
+            Console.WriteLine("Value After passing is :" + Num);
+
+            //out does'nt need an initial value of parameter
+            myProc2(out Num2);
+            Console.ReadKey();
+        }
+    }
+}
