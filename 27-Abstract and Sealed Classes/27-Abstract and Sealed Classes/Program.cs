a6bd533bcf3d9b1becfed26aaca39a7e80b65703
HAZEMADEL2030 <87597338+HAZEMADEL2030@users.noreply.github.com>
2023-10-31 23:36:42 +0200

    Add files via upload

---
+++ 27-Abstract and Sealed Classes/27-Abstract and Sealed Classes/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _27_Abstract_and_Sealed_Classes
+{
+    abstract class Person
+    {
+        public void PrintMessage()
+        {
+            Console.WriteLine("I am a person");
+        }
+    }
+
+    class Programmer : Person
+    {
+
+    }
+
+    sealed class Book
+    {
+    }
+
+    //you cannot derive from a sealed class
+    /* class ebook : Book
+     {
+     } */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Programmer prm = new Programmer();
+            prm.PrintMessage();
+        }
+    }
+}
---
+++ 28-Virtual Methods/28-Virtual Methods/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _28_Virtual_Methods
+{
+    //Main Class
+    class Person
+    {
+        //Fields
+        private string Name;
+        private int Age;
+        private string Address;
+
+        //Method
+        public void PrintMessage()
+        {
+            Console.WriteLine("I am a person");
+        }
+
+        virtual public void Work()
+        {
+            Console.WriteLine("I work as a Full Stack Developer");
+        }
+
+    }
+
+
+    //Derived Class
+    class Programmer : Person
+    {
+        public override void Work()
+        {
+            //Message From Main Class
+            base.Work();
+
+            //Message From Derived Class
+            Console.WriteLine("I am a programmer");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Programmer prm = new Programmer();
+            prm.Work();
+            Console.Read();
+        }
+    }
+}
