15bdc8e7f63e931ca98c1acab3b2e73ad1a88a0c
HAZEMADEL2030 <87597338+HAZEMADEL2030@users.noreply.github.com>
2023-10-31 23:28:19 +0200

    Add files via upload

---
+++ 23-Access Modifiers/23-Access Modifiers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _23_Access_Modifiers
+{
+
+    class Person
+    {
+        //Access Modifiers
+        private string name;
+        public string address;
+        //static
+        public static int age;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            //p.name = "Hazem";  You cannot access a private member without a property
+            p.address = "Alexandria";
+            //using static
+            Person.age = 30;
+
+            //Console.WriteLine(p.name);
+            Console.WriteLine(p.address);
+            Console.WriteLine(Person.age);
+        }
+    }
+}
---
+++ 24-Constructors/24-Constructors/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _24_Constructors
+{
+    class Car
+    {
+        //Fields
+        int ID;
+        string Color;
+        string Model;
+
+
+        //Constructors
+        public Car(int id, string color, string model)
+        {
+            this.ID = id;
+            this.Color = color;
+            this.Model = model;
+        }
+
+        public Car()
+        {
+        }
+
+        //Methods
+        public void initialize(int id, string color, string model)
+        {
+            ID = id;
+            Color = color;
+            Model = model;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine("ID is:{0}\nColor is:{1}\nModel is:{2}", ID, Color, Model);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car(1, "White", "Renault");
+            myCar.ShowDetails();
+            Console.ReadKey();
+        }
+    }
+}
