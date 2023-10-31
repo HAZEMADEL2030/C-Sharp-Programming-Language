fc869cf616736b8f2f3ad31cb978c038dcae2f4e
HAZEMADEL2030 <87597338+HAZEMADEL2030@users.noreply.github.com>
2023-10-31 23:43:22 +0200

    Add files via upload

---
+++ 31-Interfaces/31-Interfaces/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Drawing;
+
+namespace _31_Interfaces
+{
+    interface Quadrangle
+    {
+        int width { get; set; }
+        int height { get; set; }
+
+        float Area();
+    }
+
+    class Rectangle : Quadrangle
+    {
+        private int width;
+        private int height;
+
+        //Constructor with parameters
+        public Rectangle(int w, int h)
+        {
+            this.width = w;
+            this.height = h;
+        }
+
+
+        public Rectangle()
+        {
+        }
+        int Quadrangle.width
+        {
+            get { return this.width; }
+            set { this.width = value; }
+        }
+
+        int Quadrangle.height
+        {
+            get { return this.height; }
+            set { this.height = value; }
+        }
+
+        float Quadrangle.Area()
+        {
+            return this.width * this.height;
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrangle Q = new Rectangle(20, 40);
+            float CalculArea = Q.Area();
+            Console.WriteLine("The Area is:{0}", CalculArea);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 32-Delegations/32-Delegations/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _32_Delegations
+{
+    class string_manipulation
+    {
+        public static string TextCase(string Text)
+        {
+            if (Text == Text.ToUpper())
+            {
+                return "Your Text is Capital";
+            }
+            else if (Text == Text.ToLower())
+            {
+                return "Your text is Small";
+            }
+            else
+            {
+                return "Your Text is Mixed";
+            }
+        }
+
+
+        public string TextLength(string Text)
+        {
+            return string.Format("Your Text Length is: {0}", Text.Length);
+        }
+    }
+    internal class Program
+    {
+        public delegate string myDelegate(string Text);
+        static void Main(string[] args)
+        {
+            string_manipulation sm = new string_manipulation();
+            myDelegate firstDelegate = new myDelegate(sm.TextLength);
+            myDelegate secondDelegate = string_manipulation.TextCase;
+
+            Console.WriteLine("Enter a string value:");
+            string text = Console.ReadLine();
+
+            Console.WriteLine(firstDelegate.Invoke(text));
+            Console.WriteLine(secondDelegate(text));
+
+            Console.Read();
+        }
+    }
+}
