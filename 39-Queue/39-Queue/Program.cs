fcd2cb99e0616519ef13afda2088b94e94b904f2
HAZEMADEL2030 <87597338+HAZEMADEL2030@users.noreply.github.com>
2023-10-31 23:56:24 +0200

    Add files via upload

---
+++ 39-Queue/39-Queue/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace _39_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> names = new Queue<string>();
+            names.Enqueue("Hazem");
+            names.Enqueue("Adel");
+            names.Enqueue("Mohamed");
+            names.Enqueue("Ahmed");
+            names.Enqueue("Mostafa");
+
+            // A queue can be enumerated without disturbing its contents.
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("\nDequeuing '{0}'", names.Dequeue());
+            Console.WriteLine("Peek at next item to dequeue: {0}",
+                names.Peek());
+            Console.WriteLine("Dequeuing '{0}'", names.Dequeue());
+
+            // Create a copy of the queue, using the ToArray method and the
+            // constructor that accepts an IEnumerable<T>.
+            Queue<string> queueCopy = new Queue<string>(names.ToArray());
+
+            Console.WriteLine("\nContents of the first copy:");
+            foreach (string number in queueCopy)
+            {
+                Console.WriteLine(number);
+            }
+
+            // Create an array twice the size of the queue and copy the
+            // elements of the queue, starting at the middle of the
+            // array.
+            string[] array2 = new string[names.Count * 2];
+            names.CopyTo(array2, names.Count);
+
+            // Create a second queue, using the constructor that accepts an
+            // IEnumerable(Of T).
+            Queue<string> queueCopy2 = new Queue<string>(array2);
+
+            Console.WriteLine("\nContents of the second copy, with duplicates and nulls:");
+            foreach (string name in queueCopy2)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("\nqueueCopy.Contains(\"four\") = {0}",
+                queueCopy.Contains("four"));
+
+            Console.WriteLine("\nqueueCopy.Clear()");
+            queueCopy.Clear();
+            Console.WriteLine("\nqueueCopy.Count = {0}", queueCopy.Count);
+        }
+    }
+}
---
+++ 40-Lambda Expressions/40-Lambda Expressions/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _40_Lambda_Expressions
+{
+    public delegate void mydelegate();
+    class Absence
+    {
+        public event mydelegate AbsenceEvent;
+        private int Absented_Hours;
+
+        public int Absented_Hours_Property
+        {
+            get { return Absented_Hours; }
+            set { Absented_Hours = value; }
+        }
+
+        public Absence(int Hours)
+        {
+            this.Absented_Hours = Hours;
+        }
+
+        public void VerifyAbsence(int Max_Hours)
+        {
+            if (Absented_Hours > Max_Hours)
+            {
+                AbsenceEvent();
+            }
+        }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //First Example:
+            Absence absence = new Absence(10);
+            absence.AbsenceEvent += () => Console.WriteLine("Warning");
+            absence.VerifyAbsence(8);
+            Console.ReadKey();
+            //Second Example:
+            List<string> Colors = new List<string> { "Red", "Green", "Black", "Yellow", "Orange" };
+            Console.Write("Colors are:\n");
+            Colors.ForEach(kk =>
+            {
+                Console.WriteLine("\t - " + kk);
+            });
+            Console.ReadKey();
+        }
+    }
+}
