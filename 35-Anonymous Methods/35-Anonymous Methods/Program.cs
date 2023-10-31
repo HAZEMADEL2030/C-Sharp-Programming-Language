eb83bd81a622f77057051cbf90b39eae4f6a1721
HAZEMADEL2030 <87597338+HAZEMADEL2030@users.noreply.github.com>
2023-10-31 23:49:14 +0200

    Add files via upload

---
+++ 35-Anonymous Methods/35-Anonymous Methods/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _35_Anonymous_Methods
+{
+    public delegate void mydelegate();
+    class Absence
+    {
+        public event mydelegate AbsenceEvent;
+        private int Absented_Hours;
+
+        public int Absented_Hours_Property
+        {
+            get { return Absented_Hours; }
+            set { Absented_Hours = value; }
+        }
+
+        public Absence(int Hours)
+        {
+            this.Absented_Hours = Hours;
+        }
+
+        public void VerifyAbsence(int Max_Hours)
+        {
+            if (Absented_Hours > Max_Hours)
+            {
+                AbsenceEvent();
+            }
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Absence absence = new Absence(10);
+
+            absence.AbsenceEvent += delegate ()
+            {
+                Console.WriteLine("Warning !");
+            };
+
+            absence.VerifyAbsence(8);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 36-ArrayList/36-ArrayList/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+namespace _36_ArrayList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creates and initializes a new ArrayList.
+            ArrayList arraylist = new ArrayList();
+            arraylist.Add("Hazem");
+            arraylist.Add("Adel");
+            arraylist.Add("Mohamed");
+            arraylist.Add("Ahmed");
+            arraylist.Add("Mostafa");
+
+            arraylist.Remove("Mostafa");
+            arraylist.RemoveAt(3);
+            arraylist.Sort();
+
+            // Displays the properties and values of the ArrayList.
+            Console.WriteLine("arraylist");
+            Console.WriteLine("Count:{0}", arraylist.Count);
+            Console.WriteLine("Capacity:{0}", arraylist.Capacity);
+            Console.WriteLine("Values:");
+
+            foreach (var item in arraylist)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
