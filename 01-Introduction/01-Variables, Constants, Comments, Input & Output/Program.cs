b93525f9955388f3149a3569dd7cba9f88f03fe3
HAZEMADEL2030 <87597338+HAZEMADEL2030@users.noreply.github.com>
2023-10-31 22:59:25 +0200

    Add files via upload

---
+++ 01-Introduction/01-Variables, Constants, Comments, Input & Output/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _01_Variables__Constants__Comments__Input___Output
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Change The Title Of Console Window
+            Console.Title = "The First Project";
+
+            //Read a string value
+            Console.Write("Enter Your Name: ");
+            string name = Console.ReadLine();
+
+
+            //Read an integer value
+            Console.Write("Enter Your Age:");
+
+            //First Way
+            int age = int.Parse(Console.ReadLine());
+
+
+            /*Second Way:
+             * 
+            int age = Convert.ToInt16(Console.ReadLine());
+         or   int age = Convert.ToInt32(Console.ReadLine());
+           or   int age = Convert.ToInt64(Console.ReadLine());*/
+
+
+            //Print a string to output
+            Console.Write("Welcome: " + name + " Your age is: " + age + " That is Great");
+
+            //Pause the console window
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02-Operators/02-Operators/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace _02_Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1.Arithmetic Opertaors*/
+
+            //addition
+            int sum = 3 + 6;
+            //Subtraction
+            int difference = 3 - 6;
+            //multiplication
+            int product = 6 * 2;
+            //division
+            double quotient = 40 / 5;
+            //Exponentiation
+            double expo = Math.Pow(10, 7);
+            //modulus
+            double modulo = 10 % 3;
+
+            Console.WriteLine("Sum is: " + sum);
+            Console.WriteLine("Difference is: " + difference);
+            Console.WriteLine("Product is: " + product);
+            Console.WriteLine("Quotient is: " + quotient);
+            Console.WriteLine("Exponential is: " + expo);
+            Console.WriteLine("modulus is: " + modulo);
+
+
+            /*2.Comparison operators*/
+
+            //result1=true because 4 is greater than 2
+            bool result1 = 4 > 2;
+            //result2=false because 5 is not less then 1
+            bool result2 = 5 < 1;
+            //result3=true becaus 10 and 5 are not equal
+            bool result3 = 10 != 5;
+            //result4=true becaus 3 and 6/2 are equal
+            bool result4 = 3 == 6 / 2;
+
+            Console.WriteLine("Result1 is: " + result1);
+            Console.WriteLine("Result2 is: " + result2);
+            Console.WriteLine("Result3 is: " + result3);
+            Console.WriteLine("Result4 is: " + result4);
+
+
+            //3. Logical Operators
+
+            /*And (&&) operator: Performs an And operation :
+            result1 is true because all conditions are true
+            */
+            bool res1 = (2 > 1 && 4 > 2);
+
+            /*Or (||) operator: Performs an Or operation
+            result2 is true because there is a true condition
+            */
+            bool res2 = (2 > 3 || 1 < 2);
+
+
+            Console.WriteLine("Result1 is: " + res1);
+            Console.WriteLine("Result2 is: " + res2);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
