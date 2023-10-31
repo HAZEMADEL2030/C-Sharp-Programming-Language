63e08a252e76fd3a3c8fae43634c3205f39faebf
HAZEMADEL2030 <87597338+HAZEMADEL2030@users.noreply.github.com>
2023-10-31 23:20:18 +0200

    Add files via upload

---
+++ 19-Exceptions Handling/19-Exceptions Handling/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _19_Exceptions_Handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 89, b = 0;
+                double div = a / b;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executed Forever !");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 20-Console and Others/20-Console and Others/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _20_Console_and_Others
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console Properties
+            Console.Title = "C# Lesson";
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Clear();
+
+            //Escape Characters
+            Console.WriteLine("Welcome to the\tworld\nmy name is:\"Hazem\"");
+
+            //Break and Continue
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(i);
+                if (i < 10) continue; break;
+            }
+
+            //display string
+            string name = "Hazem";
+            Console.WriteLine("My name is:{0}", name);
+
+            Console.Read();
+        }
+    }
+}
