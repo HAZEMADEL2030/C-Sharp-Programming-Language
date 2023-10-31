a434ad990faa0b7e67c6f158421812d079fb0e16
HAZEMADEL2030 <87597338+HAZEMADEL2030@users.noreply.github.com>
2023-10-31 23:01:13 +0200

    Add files via upload

---
+++ 03-Flow Control If Else Statement/03-Flow Control If Else Statement/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _03_Flow_Control_If_Else_Statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+            double div;
+
+            Console.Write("Enter the First Number:");
+            number1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the Second Number:");
+            number2 = int.Parse(Console.ReadLine());
+
+            if (number2 == 0)
+            {
+                Console.Write("Cannot Divide By Zero");
+            }
+            else
+            {
+                div = number1 / number2;
+                Console.Write("The Quotient of Division is: " + div);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 04-Conditional Operator/04-Conditional Operator/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _04_Conditional_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conditional Operator
+            int number1, number2;
+
+            Console.WriteLine("Enter the first number:");
+            number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            number2 = int.Parse(Console.ReadLine());
+
+            string message = number2 == 0 ? "Cannot Divide By Zero" : "The quotient is:" + number1 / number2; ;
+
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+    }
+}
